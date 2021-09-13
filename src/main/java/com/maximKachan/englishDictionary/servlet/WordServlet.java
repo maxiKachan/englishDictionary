800919e05a5c89cf0f08220bac5ffab6b02b85c7
maxiKachan <max101111427@gmail.com>
2021-09-13 21:37:43 +0300

    add CREATE and DELETE functions

--- src/main/java/com/maximKachan/englishDictionary/servlet/WordServlet.java
+++ src/main/java/com/maximKachan/englishDictionary/servlet/WordServlet.java
@@ -7,12 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class WordServlet extends HttpServlet {
+    private static final List<String> list = DictionaryList.getAll();
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("words", DictionaryList.getAll());
+        String sId = request.getParameter("id");
+        if (sId != null && !sId.isEmpty()){
+            int id = Integer.parseInt(sId);
+            list.remove(id);
+            System.out.println(id + " removed");
+        }
         request.getRequestDispatcher("/words.jsp").forward(request, response);
+        System.out.println("get");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String word = request.getParameter("word");
+        System.out.println(word);
+        if (word != null && !word.isEmpty()){
+            list.add(word);
+            System.out.println("add word");
+        }
+        response.sendRedirect("words");
     }
 }
--- src/main/webapp/words.jsp
+++ src/main/webapp/words.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html;charset=UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
@@ -6,13 +6,23 @@
 </head>
 <body>
 <h1>Words JSP</h1>
+<form action="words" method="post">
+    <h4>Add new words</h4>
+    <label><input type="text" name="word" required></label>
+    <button type="submit">Add</button>
+</form>
 <table>
     <tr>
-        <th>Word</th>
+        <th>Words</th>
+        <th>action</th>
     </tr>
     <jsp:useBean id="words" scope="request" type="java.util.List"/>
     <c:forEach var="list" items="${words}">
-        <tr><td>${list}</td></tr>
+        <tr>
+            <td>${list}</td>
+            <td><a href="words?id=${words.indexOf(list)}">Delete</a></td>
+        </tr>
+
     </c:forEach>
 </table>
 </body>
